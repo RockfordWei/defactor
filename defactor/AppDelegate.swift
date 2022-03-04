e110cc0a60fb4723f1521d12086217d7605c4632
Rockford Wei <rocky@perfect.org>
2022-03-04 08:49:36 -0500

    init

---
+++ defactor/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  defactor
+//
+//  Created by Rockford Wei on 2022-03-04.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
